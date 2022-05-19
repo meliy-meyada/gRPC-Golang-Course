8306b0af62a6b004835c86be645a8506fb6d895c
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-19 20:00:35 +0700

    Server Setup Boilerplate Code

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to Listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on: %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
