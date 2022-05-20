b0c0b403c40a849ab61ee6f598c806d8f35c6021
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-20 14:59:43 +0700

    Exercise Sum API

---
+++ calculator/client/sum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/calculator/proto"
+)
+
+
+func doSum(c pb.CalculatorServiceClient){
+	log.Println("doSum was invoked")
+	res, err := c.Sum(context.Background(), &pb.SumRequest{
+		FirstNumber: 1,
+		SecondNumber: 1,
+	})
+	if err != nil {
+		log.Fatalf("Could not sum: %v", err)
+	}
+
+	log.Printf("Sum: %d\n", res.Result)
+}
---
+++ calculator/server/sum.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/calculator/proto"
+)
+
+
+func (s *Server) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error){
+	log.Printf("Sum function was invoked with %v\n", in)
+	return &pb.SumResponse{
+		Result: in.FirstNumber + in.SecondNumber,
+	}, nil
+}
