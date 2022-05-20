04a815cdfb484d9a68060d5373edf208b65d3cf7
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-20 17:35:37 +0700

    Client Streaming API Server Implementation

---
+++ greet/server/long_greet.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/greet/proto"
+)
+
+
+func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error{
+	log.Println("LongGreet Function was invoked")
+
+	res := ""
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.GreetResponse{
+				Result: res,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+		res += fmt.Sprintf("Hello %s!\n", req.FirstName)
+	}
+}
