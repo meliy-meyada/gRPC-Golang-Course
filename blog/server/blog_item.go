8c0c4565d9b3e4c43d578e6ba24290fde99599a3
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-26 15:02:32 +0700

    CreateBlog Server

---
+++ blog/server/blog_item.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorId string `bson:"author_id"`
+	Title string `bson:"title"`
+	Content string`bson:"content"`
+}
+
+func documemtToBlog(data *BlogItem) *pb.Blog {
+	return &pb.Blog{
+		Id: data.ID.Hex(),
+		AuthorId: data.AuthorId,
+		Title: data.Title,
+		Content: data.Content,
+	}
+}
---
+++ blog/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/blog/proto"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+)
+
+var collection *mongo.Collection
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.BlogServiceServer
+}
+
+func main() {
+	client, err :=  mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection =  client.Database("blogdb").Collection("blog")
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to Listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on: %s\n", addr)
+
+	s := grpc.NewServer()
+	pb.RegisterBlogServiceServer(s, &Server{})
+
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
