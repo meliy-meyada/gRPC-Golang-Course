9ef4f0d809ba059073b4c14576bbbe01c1c612ee
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-26 17:02:52 +0700

    ListBlog Server

---
+++ blog/server/list.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) ListBlogs(in *emptypb.Empty, stream pb.BlogService_ListBlogsServer) error {
+	log.Println("ListBlogs was invoked")
+
+	cur, err  := collection.Find(context.Background(), primitive.D{{}})
+
+	if err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unknown internal error: %v", err),
+		)
+	}
+
+	defer cur.Close(context.Background())
+
+	for cur.Next(context.Background()) {
+		data := &BlogItem{}
+		err :=  cur.Decode(data)
+
+		if err != nil {
+			return status.Errorf(
+				codes.Internal,
+				fmt.Sprintf("Error while docoding data from MongoDB: %v", err),
+			)
+		}
+		stream.Send(documemtToBlog(data))
+	}
+
+	if err = cur.Err(); err != nil {
+			return status.Errorf(
+				codes.Internal,
+				fmt.Sprintf("Unknown internal error: %v", err),
+			)
+	}
+	return nil
+}
