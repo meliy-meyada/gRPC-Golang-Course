26f204d33cff22e4cbfda4d9feec6e915927fa75
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-26 17:15:38 +0700

    ListBlog Client

---
+++ blog/client/list.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/meliy-meyada/gRPC-Golang-Course/blog/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func listBlog(c pb.BlogServiceClient) {
+	log.Println("---listBlog was invokeed---")
+
+	stream, err := c.ListBlogs(context.Background(), &emptypb.Empty{})
+
+	if err != nil {
+		log.Fatalf("Error while calling ListBlogs: %v\n", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Something happened: %v\n", err)
+		}
+
+		log.Println(res)
+	}
+}
--- blog/client/main.go
+++ blog/client/main.go
@@ -27,6 +27,7 @@ func main(){
 	readBlog(c, id) // valid
 	// readBlog(c, "aNonExistingId") // invalid
 	updateBlog(c, id)
+	listBlog(c)
 
 
 }
